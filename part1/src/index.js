3ccc288d32f1e2ed1a47aee7e813222dff74e1f9
Anibal Jose Rodriguez <arorive@protonmail.com>
2021-11-28 17:41:03 -0600

    I finished exercises 1.6, 1.7, 1.8, 1.9, 1.10, 1.11

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,59 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>
-}
 
-const Content  = (props) => {
+const Statistic = (props) => {
+  return (
+      <tr><td>{props.text}</td><td>{props.value}</td></tr>
+  )
+}
+const Statistics = (props) => {
+  const averagePositive = (props.objState.good  / props.objState.allState) * 100;
+  const averageScore = props.objState.allState / 3;
+
+  if (props.objState.allState > 0) {
+    return (
+      <div>
+        <br/><strong>Statistics </strong><br/>
+        <table>
+          <tbody>
+            <Statistic text={"good"}     value={props.objState.good}/>
+            <Statistic text={"neutral"}  value={props.objState.neutral}/>
+            <Statistic text={"bad"}      value={props.objState.bad}/>
+            <Statistic text={"All"}      value={props.objState.allState}/>
+            <Statistic text={"Avarage"}  value={averageScore}/>
+            <Statistic text={"Positive"} value={averagePositive}/>
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
   return (
     <div>
-      <Part part={props.parts[0]}  />
-      <Part part={props.parts[1]}  />
-      <Part part={props.parts[2]}  />
+      No feedBack Given
     </div>
+
   )
 }
 
-const Part = (props => {
-  return (
-    <p>{props.part.name} {props.part.exercises}</p>
-  )
-})
-
-const Total  = (props) => {
-  return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-}
-
-
+const Button = ({ onClick, text }) => (  <button onClick={onClick}>    {text}  </button>)
 
 const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
+  const [objState, setObjState] = useState({ good:0, neutral:0, bad:0, allState:0 })
+
+  const addGood = () => {
+    const newStates = {
+      ...objState,
+      good: objState.good + 1,
+      allState: objState.allState + 1
+    }
+    setObjState(newStates);
+  }
+
+  const addNeutral = () => {
+    const newStates = {
+      ...objState,
+      neutral: objState.neutral + 1,
+      allState: objState.allState + 1
+    }
+    setObjState(newStates);
+  }
+
+  const addBad = () => {
+    const newStates = {
+      ...objState,
+      bad: objState.bad + 1,
+      allState: objState.allState + 1
+    }
+    setObjState(newStates);
   }
   return (
-    
     <div>
-      <Header  course ={course.name} /> 
-      <Content parts  ={course.parts}/>
-      <Total   parts  ={course.parts} />
+      <Button onClick={addGood} text='Good' />      
+      <Button onClick={addNeutral} text='Neutral' />   
+      <Button onClick={addBad} text='Bad' />   
+      <Statistics objState={objState} ></Statistics>
     </div>
   )
 }
 
-
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
